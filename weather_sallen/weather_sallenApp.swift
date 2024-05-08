0bbbf7d379e89cfbd8bb4a02c843b112bc2fe9e7
Adam Kaum <adam.bolotov@gmail.com>
2024-05-09 01:28:21 +0400

    Initial Commit

---
+++ weather_sallen/weather_sallenApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_sallenApp.swift
+//  weather_sallen
+//
+//  Created by Adam Kaum on 09.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct weather_sallenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
