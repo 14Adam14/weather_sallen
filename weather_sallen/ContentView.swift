6d26263b3ba29214505ea5d7779394614ea03edb
Adam Kaum <adam.bolotov@gmail.com>
2024-05-09 03:45:15 +0400

    55 min

--- weather_sallen/ContentView.swift
+++ weather_sallen/ContentView.swift
@@ -11,15 +11,15 @@ struct ContentView: View {
         
         ZStack {
             
-            LinearGradient(colors: [.blue, .white], startPoint: .topLeading, endPoint: .bottomTrailing)
+            LinearGradient(colors: [.blue, .lightBlue], startPoint: .topLeading, endPoint: .bottomTrailing)
                 .ignoresSafeArea()
             
             
             VStack{
                 Text("Cupertino, CA")
-                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .font(.system(size: 34, weight: .medium, design: .default))
                     .foregroundStyle(.white)
-                    .padding()
+                    .padding(40)
                 
                 
                 VStack(spacing: 10) {
@@ -27,23 +27,53 @@ struct ContentView: View {
                         .renderingMode(.original)
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                        .frame(width: 180, height: 180)
-                        
+                        .frame(width: 190, height: 190)
+                    
                     
                     Text("76°")
-                        .font(.system(size: 70, weight: .medium))
+                        .font(.system(size: 74, weight: .medium))
                         .foregroundStyle(.white)
                     
+                }
+                .padding(.bottom, 45)
                     
-                    HStack{
-                        WeatherDayView(dayOfWeek: "TUE", imageName: "cloud.sun.fill", tempereture: 76)
+                    
+                    HStack(spacing: 26) {
+                        WeatherDayView(dayOfWeek: "TUE",
+                                       imageName: "cloud.sun.fill",
+                                       tempereture: 69)
+                        WeatherDayView(dayOfWeek: "WED",
+                                       imageName: "sun.max.fill",
+                                       tempereture: 76)
+                        WeatherDayView(dayOfWeek: "THU",
+                                       imageName: "wind.snow",
+                                       tempereture: 48)
+                        WeatherDayView(dayOfWeek: "FRI",
+                                       imageName: "sunset.fill",
+                                       tempereture: 55)
+                        WeatherDayView(dayOfWeek: "SAT",
+                                       imageName: "cloud.moon.bolt.fill",
+                                       tempereture: 51)
                     }
                     
-                    
-                    
                     Spacer()
+                    
+                    
+                
+                Button(action: {
+                    print("dsd")
+                }, label: {
+                    Text("Change Day Time")
+                        .frame(width: 290, height: 60)
+                        .background(.white)
+                        .font(.system(size: 20, weight: .bold, design: .default))
+                        .clipShape(RoundedRectangle(cornerRadius: 15))
+                })
+                
+                
+                   Spacer()
 
-                }
+                
                 
             }
             
@@ -71,17 +101,17 @@ struct WeatherDayView: View {
     var body: some View {
         VStack{
             Text(dayOfWeek)
-                .font(.system(size: 16, weight: .medium, design: .default))
+                .font(.system(size: 17, weight: .medium, design: .default))
                 .foregroundStyle(.white)
             
             Image(systemName: imageName)
                 .renderingMode(.original)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
-                .frame(width: 40, height: 40)
+                .frame(width: 39, height: 39)
             
             Text("\(tempereture)°")
-                .font(.system(size: 28, weight: .medium, design: .default))
+                .font(.system(size: 29, weight: .medium, design: .default))
                 .foregroundStyle(.white)
         }
     }
